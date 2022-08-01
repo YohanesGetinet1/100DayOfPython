31540c6698f1eeef708a5f49d83fe195c2ef550e
Yohanes Getinet Nuriye <johnget496@gmail.com>
2022-08-01 18:28:51 +0530

    100 day of python first day project project name = band name generator @Author Yohanes Getinet @Date 01/08/2022

---
+++ band_name_Generator.py
@@ -0,0 +1,5 @@
+# First day project
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in? \n")
+pit_name = input("What's your pet's name? \n")
+print("Your band name could be " + city_name + " " + pit_name)
