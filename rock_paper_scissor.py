ed5e8e7f92c147fc0a51e183bfac4ca95c5fbe8b
Yohanes Getinet Nuriye <johnget496@gmail.com>
2022-08-04 21:52:58 +0530

    100DayofPython Day 4 Project @Author Yohanes Getinet @Date 04/08/2022

---
+++ rock_paper_scissor.py
@@ -0,0 +1,54 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
+
+print(images[user_choice])
+computer_choice = random.randint(0,2)
+print(images[computer_choice])
+
+if user_choice < 0 or user_choice >= 3:
+  print("You have entered invalid number")
+elif user_choice == 0 and computer_choice == 2:
+  print("You win")
+elif user_choice == 1 and computer_choice == 0:
+  print("You win")
+elif user_choice == 2 and computer_choice == 1:
+  print("You win")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice == 1 and user_choice == 0:
+  print("You lose")
+elif computer_choice == 2 and user_choice == 1:
+  print("You lose")
+elif computer_choice == user_choice:
+  print("You draw")
